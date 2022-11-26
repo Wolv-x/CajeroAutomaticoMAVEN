ecbd444c64184660e0e646b2106cd585b165751e
Dennis Lincango <dennis.lincango@epn.edu.ec>
2022-11-26 16:38:29 -0500

    AssertFalse en TarjetaTest

--- src/test/java/ec/edu/epn/cajero/cuenta/TarjetaTest.java
+++ src/test/java/ec/edu/epn/cajero/cuenta/TarjetaTest.java
@@ -2,18 +2,21 @@ package ec.edu.epn.cajero.cuenta;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TarjetaTest {
 
     @Test
-    public void given_creditCardNumber_when_validLenght_then_OK() {
+    public void given_creditCardNumber_when_validLenghtAndValidPassword_then_OK() {
         Tarjeta tarjeta = new Tarjeta();
         assertTrue(tarjeta.validarTarjeta("4271900557374142", "123"));
     }
 
     @Test
-    public void otraPrueba6() {
-
+    public void given_creditCardNumber_when_validLenghtAndInvalidPassword_then_OK() {
+        Tarjeta tarjeta = new Tarjeta();
+        assertFalse(tarjeta.validarTarjeta("4271900557374142", "12333123123"));
     }
+
 }
