ae69a9c17528abfe6251c6502107348fed387c4a
Dennis Lincango <dennis.lincango@epn.edu.ec>
2022-11-26 18:44:46 -0500

    Refactorización 01: Extract method

--- src/test/java/ec/edu/epn/cajero/deposit/DepositProcessorTest.java
+++ src/test/java/ec/edu/epn/cajero/deposit/DepositProcessorTest.java
@@ -5,20 +5,22 @@ import static org.junit.Assert.*;
 
 public class DepositProcessorTest {
 
+    private DepositGateway gateway = null;
+    private DepositProcessor processor = null;
+
+    public void setUp() {
+        gateway = Mockito.mock(DepositGateway.class);
+        processor = new DepositProcessor(gateway);
+    }
+
     @Test
     public void give_deposit_when_is_correct_then_OK() {
-        DepositGateway gateway = Mockito.mock(DepositGateway.class);
-        DepositProcessor processor = new DepositProcessor(gateway);
-
         Mockito.when(gateway.requestDeposit(Mockito.any())).thenReturn(new DepositResponse(DepositResponse.DepositStatus.OK));
         assertTrue(processor.makeDeposit(245));
     }
 
     @Test
     public void give_deposit_when_is_wrong_then_ERROR() {
-        DepositGateway gateway = Mockito.mock(DepositGateway.class);
-        DepositProcessor processor = new DepositProcessor(gateway);
-
         Mockito.when(gateway.requestDeposit(Mockito.any())).thenReturn(new DepositResponse(DepositResponse.DepositStatus.ERROR));
         assertFalse(processor.makeDeposit(480));
     }
