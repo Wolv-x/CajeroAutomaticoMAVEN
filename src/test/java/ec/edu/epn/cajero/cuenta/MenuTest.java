aa8547d83ce606638dafceccdb755e50397b47c6
Dennis Lincango <dennis.lincango@epn.edu.ec>
2022-11-26 18:17:22 -0500

    Limpieza de código

--- src/test/java/ec/edu/epn/cajero/cuenta/MenuTest.java
+++ src/test/java/ec/edu/epn/cajero/cuenta/MenuTest.java
@@ -1,21 +1,19 @@
 package ec.edu.epn.cajero.cuenta;
 
-import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 
 import static org.junit.Assert.assertNotNull;
 
 public class MenuTest {
-    Menu menu=null;
-    @Before
-    public void setUp() {
-        menu = new Menu();
-        System.out.println("Setup");
-    }
     @Test
     public void given_aMenuObject_when_aNotNullObject_then_OK() {
+        Menu menu = new Menu();
         assertNotNull(menu);
     }
+    @Test
+    public void otraPrueba4() {
 
+    }
 }
