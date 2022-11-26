a822fbbb2c4cb6f16b90346689a7c6aa6944f8c9
Evo124 <gabrielnazate124@gmail.com>
2022-11-26 17:05:22 -0500

    Test: assertNotNull en la clase MenuTest, Christian Nazate

--- src/test/java/ec/edu/epn/cajero/cuenta/MenuTest.java
+++ src/test/java/ec/edu/epn/cajero/cuenta/MenuTest.java
@@ -2,10 +2,15 @@ package ec.edu.epn.cajero.cuenta;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+
+import static org.junit.Assert.assertNotNull;
+
 public class MenuTest {
     @Test
-    public void otraPrueba3() {
-
+    public void given_aMenuObject_when_aNotNullObject_then_OK() {
+        Menu menu = new Menu();
+        assertNotNull(menu);
     }
     @Test
     public void otraPrueba4() {
