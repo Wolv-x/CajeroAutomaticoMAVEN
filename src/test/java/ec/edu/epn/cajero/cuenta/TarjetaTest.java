0acf8d7aacd6a13ebfd0eb0d2a98bc9b7eb2f51f
Dennis Lincango <dennis.lincango@epn.edu.ec>
2022-11-26 16:25:21 -0500

    AssertTrue en Tarjeta Test

--- src/test/java/ec/edu/epn/cajero/cuenta/TarjetaTest.java
+++ src/test/java/ec/edu/epn/cajero/cuenta/TarjetaTest.java
@@ -2,11 +2,16 @@ package ec.edu.epn.cajero.cuenta;
 
 import org.junit.Test;
 
-public class TarjetaTest {
-    @Test
-    public void otraPrueba5() {
+import static org.junit.Assert.assertTrue;
 
+public class TarjetaTest {
+
+    @Test
+    public void given_creditCardNumber_when_validLenght_then_OK() {
+        Tarjeta tarjeta = new Tarjeta();
+        assertTrue(tarjeta.validarTarjeta("4271900557374142", "123"));
     }
+
     @Test
     public void otraPrueba6() {
 
