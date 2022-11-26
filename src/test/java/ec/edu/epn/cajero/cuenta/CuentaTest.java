ef9125eae84af14d165ff0d57e882b6e89cca933
Evo124 <gabrielnazate124@gmail.com>
2022-11-26 18:12:09 -0500

    Before clase CuentaTest, Christian Nazate

--- src/test/java/ec/edu/epn/cajero/cuenta/CuentaTest.java
+++ src/test/java/ec/edu/epn/cajero/cuenta/CuentaTest.java
@@ -14,7 +14,6 @@ public class CuentaTest {
     }
     @Test
      public void given_aBalance_when_consultarSaldo_then_ok() {
-        //Cuenta cuenta = new Cuenta();
         float expected = 27.2F;
         float actual = cuenta.consultarSaldo("4271900557374142");
         assertEquals(expected, actual,0.0F);
